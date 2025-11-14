b4da97999f2279db45fc0d73f767669e3faef98f
Vicente Neto <31258486+VicenteNeto21@users.noreply.github.com>
2025-11-14 13:25:51 -0300

    Delete rodape.php

--- rodape.php
+++
@@ -1,4 +0,0 @@
-<footer class="bg-blue-700 text-white text-center py-4 mt-10">
-    <p class="text-sm">&copy; <?= date("Y"); ?> HVTPETSHOP - Todos os direitos reservados.</p>
-    <p class="text-sm">Desenvolvido com ☕ por <a href="https://seusite.com" class="underline hover:text-gray-300">Vicente Neto</a></p>
-</footer>
